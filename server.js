0fe7ba45fec670e828dfbda6fdb0bbb9f4f7a0c3
Puneet Ahuja <puneetahuja@adobe.com>
2021-12-28 14:04:35 +0530

    Registered GraphQL with Express

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const  { graphqlHTTP: expressGraphQL } = require('express-graphql');
+
 
 const app = express();
+
+// Registering Route - /graphql to the function returned from the expressGraphQL
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
 
 
 const PORT = 4000;
